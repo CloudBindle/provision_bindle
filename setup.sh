4cdef9a1c07e11b556ebfc59172c9d2b24242dfb
Denis Yuen <denis.yuen@oicr.on.ca>
2014-11-19 15:39:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ sudo apt-get -y install python-software-properties
 sudo add-apt-repository --yes ppa:rquillo/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
-git clone -b 2.0-alpha.2 https://github.com/CloudBindle/Bindle.git playbooks/Bindle
+git clone https://github.com/CloudBindle/Bindle.git playbooks/Bindle
+cd playbooks/Bindle
+git checkout 2.0-alpha.2
+cd ../..
 
