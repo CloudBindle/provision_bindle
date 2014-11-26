9935647ae68394dc9515eef23f768b10ba850c5e
nialloicr <Niall.Byrne@oicr.on.ca>
2014-11-26 17:35:36 -0500

    Update setup.sh
    On 14.04, an initial apt-get update is required before installing 'python-software-properties'.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get -y install git
-sudo apt-get -y install python-software-properties
 sudo add-apt-repository --yes ppa:rquillo/ansible
 sudo apt-get update
+sudo apt-get -y install python-software-properties
 sudo apt-get -y install ansible
 git clone https://github.com/CloudBindle/Bindle.git playbooks/Bindle
 cd playbooks/Bindle
