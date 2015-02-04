88be37ce2bb94b811665c40ed8dc38741ee26b3e
SolomonShorser-OICR <Solomon.Shorser@oicr.on.ca>
2015-02-04 11:43:39 -0500

    Added new dependencies for vagrant-aws, nokogiri
    New vagrant-aws was having problems installing nokogiri, due to missing dependencies. Solution is adding packages referenced in this comment: https://github.com/mitchellh/vagrant-aws/issues/163#issuecomment-27603855

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ sudo add-apt-repository --yes ppa:rquillo/ansible
 sudo apt-get update
 sudo apt-get -y install python-software-properties
 sudo apt-get -y install ansible
+sudo apt-get -y install build-essential
+sudo apt-get -y install libxslt1-dev
+sudo apt-get -y install libxml2-dev
+sudo apt-get -y install zlib1g-dev
 git clone https://github.com/CloudBindle/Bindle.git playbooks/Bindle
 cd playbooks/Bindle
 git checkout 2.0-alpha.4
