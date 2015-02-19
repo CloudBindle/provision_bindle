a607a818d7573106438ce5d903a7ac98da3c4a80
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-02-19 11:30:25 -0500

    New script to help update worker nodes This script will determine which worker nodes are available to have an new version of a worklow pushed to them.

---
+++ get_nodes_for_update.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#TODO: This should value should come from user input
+#WORKFLOW_VERSION="1.0.4"
+WORKFLOW_VERSION=$1
+#TODO: This should value should come from user input
+#WORKFLOW_NAME="Workflow_Bundle_"
+WORKFLOW_NAME=$2
+# Probably several ways to get a list of worker nodes, but maybe best to just ask vagrant directly.
+# Get a list of everything that varant is managing (that's also in the architecture2/Bindle directory)
+# $NODES will be an array of directories of vagrant-managed workers.
+echo "Getting list of workers from Vagrant"
+NODES=($(vagrant global-status | grep .*/Bindle/.* | sed 's/.*\(\/home\/ubuntu\/architecture2\/Bindle\/\).*/\1/g'))
+
+if [ ${#NODES[@]} = 0 ] ; then
+  echo "Vagrant is not aware of any running nodes. Exiting..."
+  exit
+fi
+
+# Write an inventory file containing all nodes that need to be checked to see if they can be updated.
+#echo "[master]" > candidate_nodes_for_update_inventory
+n=1
+echo "" > candidate_nodes_for_update_inventory
+for node_dir in "${NODES[@]}"
+do
+  echo "[host_$n]" >> candidate_nodes_for_update_inventory
+  NODE_INVENTORY=$(cat "$node_dir"*/inventory | grep ansible_ssh_host)
+  echo $NODE_INVENTORY >> candidate_nodes_for_update_inventory
+  n=$n+1
+done
+
+# Need to first copy status check script template.
+cd ~/architecture2/monitoring-bag
+cp roles/client/templates/check-seqware-sanger-last-workflow-status.json.j2 ./check-seqware-sanger-last-workflow-status.sh
+# replace the Ansible variable with a real value.
+sed -i 's/{{ workflow_version }}/'$WORKFLOW_VERSION'/g' check-seqware-sanger-last-workflow-status.sh
+# George's script always searches for workflows prefixed with "Workflow_Bundle_" but this line
+# modifies the script to search for ANY workflow name.
+sed -i 's/Workflow_Bundle_'$WORKFLOW_VERSION'/'$WORKFLOW_NAME'/g' check-seqware-sanger-last-workflow-status.sh
+
+# Tell ansible to run the script for all machines in nodes_for_update_inventory.
+NODES_TO_UPDATE=()
+echo "Checking for workers that have completed their work..."
+for ((i=1; i<=${#NODES[@]}; i++));
+do
+  echo "Checking node:"
+  HOST_LINE=$(grep -A 1 -e "\[host_$i\]" ~/architecture-setup/candidate_nodes_for_update_inventory | sed 's/\[host_'$i'\]//g')
+  echo $HOST_LINE
+  ANSIBLE_RESULT=$(ansible -v -i ~/architecture-setup/candidate_nodes_for_update_inventory host_$i -m script -a check-seqware-sanger-last-workflow-status.sh)
+  ANSIBLE_STD_OUT=$(echo $ANSIBLE_RESULT |  sed 's/.*"stdout": "\([^"]*\)".*/\1/g')
+  ANSIBLE_STD_ERR=$(echo $ANSIBLE_RESULT |  sed 's/.*"stderr": "\([^"]*\)".*/\1/g')
+  if [[ $ANSIBLE_OUT =~ .*completed.* ]]
+  then 
+    echo "Worker has completed and will be added to the inventory for nodes that will be updated."
+    NODES_TO_UPDATE+=("$HOST_LINE")
+  else
+    echo "Worker has not completed, message received by Ansible: $ANSIBLE_STD_OUT"
+    if [ ! $ANSIBLE_STD_ERR = '' ] ; then 
+      echo "Std Err: $ANSIBLE_STD_ERR"
+    fi
+  fi
+done
+
+if [ ${#NODES_TO_UPDATE[@]} > 0 ] ; then
+  echo "[master]" > ~/architecture2/pancancer-bag/nodes_to_update_inventory
+  for node_line in "${NODES_TO_UPDATE[@]}"
+  do  
+    echo $node_line >> ~/architecture2/pancancer-bag/nodes_to_update_inventory
+  done
+fi
+ 
