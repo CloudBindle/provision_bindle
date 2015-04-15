9fbee723c843640bae24d7d850608f7c982799e4
Brian O'Connor <briandoconnor@gmail.com>
2015-04-15 14:49:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,10 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install libxslt1-dev
 sudo apt-get -y install libxml2-dev
 sudo apt-get -y install zlib1g-dev
+git clone https://github.com/ICGC-TCGA-PanCancer/architecture-setup.git
+cd architecture-setup 
 git clone https://github.com/CloudBindle/Bindle.git playbooks/Bindle
 cd playbooks/Bindle
 git checkout 2.0-rc.0
 cd ../..
-
+ansible-playbook -i inventory site.yml
