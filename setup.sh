2328f254e132cd05dadbbde575590173c2bbf41f
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-04-21 13:57:26 -0400

    Update Bindle version; add repo for newer version of ansible

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get -y install git
 sudo add-apt-repository --yes ppa:rquillo/ansible
+sudo add-apt-repository --yes ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install python-software-properties
 sudo apt-get -y install ansible
@@ -16,6 +17,6 @@ git clone https://github.com/ICGC-TCGA-PanCancer/architecture-setup.git
 cd architecture-setup 
 git clone https://github.com/CloudBindle/Bindle.git playbooks/Bindle
 cd playbooks/Bindle
-git checkout 2.0-rc.0
+git checkout release/2.0.0
 cd ../..
 ansible-playbook -i inventory site.yml
