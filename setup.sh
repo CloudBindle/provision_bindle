25420e6ce031dc8fa05a9f43736081443378b94c
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-04-22 12:04:37 -0400

    New code for Ansible 1.9; Update workflow-decider version

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
 sudo apt-get -y install git
 sudo add-apt-repository --yes ppa:rquillo/ansible
 sudo add-apt-repository --yes ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install python-software-properties
-sudo apt-get -y install ansible
+
+# Make sure we get Ansible 1.9.*
+ANSIBLE_VERSION=$(apt-cache showpkg ansible | grep "^1\.9[^[:space:]]*" | sed 's/^\(1\.9[^ ]*\) .*$/\1/' | tail -1)
+echo "Using Ansible version: $ANSIBLE_VERSION"
+[ -z "$ANSIBLE_VERSION" ] && echo "Could not detect Ansible 1.9.*, available versions are: " && apt-cache showpkg ansible && exit
+sudo apt-get -y install ansible=$ANSIBLE_VERSION
+
 sudo apt-get -y install build-essential
 sudo apt-get -y install libxslt1-dev
 sudo apt-get -y install libxml2-dev
