abeb7fcada11bd29708b24c2bb22fdc52c459d62
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-05-06 17:46:41 -0400

    Changes to how images are built  - Dockerfile generator now allows you to include a tag  - Split into two images: seqware/launcher_base and seqware/launcher  - script to launch containers also takes argument for image label.

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -6,14 +6,15 @@
 # 3) Checkout out the repo on the build machine and copying local files into the docker container
 
 LAUNCHER_VERSION=$1
-REF=$2
+BASE_VERSION=$2
+REF=$3
 BUILD_DATE=$(date +"%y-%m-%d %H:%M:%S")
 
 read -r -d '' COMMON<<'STARTBLOCK'
 # Based on Ubuntu 14
 FROM ubuntu:14.04
 MAINTAINER Solomon Shorser <solomon.shorser@oicr.on.ca>
-LABEL architecture-setup=ARCH_SETUP_VERSION build-date=IMAGE_BUILD_DATE
+LABEL architecture-setup=3.0.0 build-date=15-05-05 18:01:22
 
 # some packages needed by the other bags needed packages in "precise" but not in "trusty". Specifically, libdb4.8 was needed.
 RUN apt-get install -y software-properties-common && \
@@ -44,23 +45,35 @@ ENV PYTHONUNBUFFERED 1
 STARTBLOCK
 
 read -r -d '' BUILD_FROM_SCRIPT <<'SCRIPTBLOCK'
-RUN curl -L https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/architecture-setup/develop/setup.sh | bash
+FROM seqware/launcher_base:BASE_VERSION
+RUN curl -L https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/architecture-setup/GIT_REF/setup.sh | bash
+WORKDIR /home/ubuntu/architecture-setup
 SCRIPTBLOCK
 
 read -r -d '' BUILD_FROM_CHECKOUT <<'CHECKOUTBLOCK'
+FROM seqware/launcher_base:BASE_VERSION
 RUN git clone https://github.com/ICGC-TCGA-PanCancer/architecture-setup.git && \
     cd architecture-setup && \
     git checkout GIT_REF && \
-    git submodule init && git submodule update && \ 
-    cd youxia/youxia-setup &&
+    git submodule init && git submodule update
+WORKDIR /home/ubuntu/architecture-setup
+RUN ansible-playbook -i inventory site.yml
+RUN cd youxia/youxia-setup && \
+    ansible-playbook -i inventory site.yml
+RUN cd youxia/ansible_sensu && \
     ansible-playbook -i inventory site.yml
 CHECKOUTBLOCK
 
 read -r -d '' BUILD_FROM_LOCAL_SOURCE <<'LOCALSRCBLOCK'
+FROM seqware/launcher_base:BASE_VERSION
 # This assumes you will run the docker build command from your local architecture-setup directory
 ADD . /home/ubuntu/architecture-setup
 WORKDIR /home/ubuntu/architecture-setup
 RUN bash setup.sh
+RUN cd youxia/youxia-setup && \
+    ansible-playbook -i inventory site.yml
+RUN cd youxia/ansible_sensu && \
+    ansible-playbook -i inventory site.yml
 LOCALSRCBLOCK
 
 read -r -d '' END <<'ENDBLOCK'
@@ -69,17 +82,33 @@ ENDBLOCK
 
 COMMON=$(echo "$COMMON" | sed s/'ARCH_SETUP_VERSION'/$LAUNCHER_VERSION/g )
 COMMON=$(echo "$COMMON" | sed s/'IMAGE_BUILD_DATE'/"$BUILD_DATE"/g )
+COMMON=$(echo "$COMMON" | sed s/'BASE_VERSION'/"$BASE_VERSION"/g )
 
-DOCKERFILE="$COMMON\n$BUILD_FROM_SCRIPT\n$END"
-echo -e "$DOCKERFILE" > Dockerfile.script.tmp
+DOCKERFILE="$COMMON\n"
+echo -e "$DOCKERFILE" > launcher_base/Dockerfile
 
-DOCKERFILE="$COMMON\n$BUILD_FROM_CHECKOUT\n$END"
+DOCKERFILE="$BUILD_FROM_SCRIPT\n$END\n"
 DOCKERFILE=${DOCKERFILE//GIT_REF/"$REF"}
-echo -e "$DOCKERFILE" > Dockerfile.checkoutincontainer.tmp
+DOCKERFILE=$(echo "$DOCKERFILE" | sed s/'BASE_VERSION'/"$BASE_VERSION"/g )
+echo -e "$DOCKERFILE" > launcher_rest/Dockerfile.script.tmp
 
-DOCKERFILE="$COMMON\n$BUILD_FROM_LOCAL_SOURCE\n$END"
-echo -e "$DOCKERFILE" > Dockerfile.localsrc.tmp
+DOCKERFILE="$BUILD_FROM_CHECKOUT\n$END\n"
+DOCKERFILE=${DOCKERFILE//GIT_REF/"$REF"}
+DOCKERFILE=$(echo "$DOCKERFILE" | sed s/'BASE_VERSION'/"$BASE_VERSION"/g )
+echo -e "$DOCKERFILE" > launcher_rest/Dockerfile.checkoutincontainer.tmp
 
-#docker build -t seqware/launcher --rm=true --no-cache=true .
+DOCKERFILE="$BUILD_FROM_LOCAL_SOURCE\n$END\n"
+DOCKERFILE=$(echo "$DOCKERFILE" | sed s/'BASE_VERSION'/"$BASE_VERSION"/g )
+echo -e "$DOCKERFILE" > launcher_rest/Dockerfile.localsrc.tmp
 
-#docker build -t seqware/launcher .
+#docker build -t seqware/launcher:"$LAUNCHER_VERSION" --rm=true --no-cache=true .
+
+#docker build -t seqware/launcher:"$LAUNCHER_VERSION" .
+echo "If you wish to build the base image, do this: "
+echo "$ cd launcher_base ; docker build -t seqware/launcher_base:$LAUNCHER_VERSION ."
+echo ""
+echo "Once you have a base image, copy the file you wish to use to build the docker image from \"launcher_rest\" to this directory and then run it."
+echo "Example:"
+echo "$ cp launcher_rest/Dockerfile.checkoutincontainer.tmp ./Dockerfile"
+echo "$ docker build -t seqware/launcher:$LAUNCHER_VERSION ."
+
--- launch_docker.sh
+++ launch_docker.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/bash
+
 PEM_KEY=$1
+IMAGE_VERSION=$2
+
 # Create a folder that will be mounted into the docker container
 [[ -d ~/ssh_for_docker ]] || mkdir ~/ssh_for_docker
 # Copy the pem file in $1 to the folder for the container.
 cp $PEM_KEY ~/ssh_for_docker/$(basename $PEM_KEY)
 # After running this command, you will have to run "sudo docker attach launcher" to get into the container.
-docker --storage-driver=devicemapper run -i -t -d -P  --privileged=true --name launcher -v /home/$USER/ssh_for_docker:/opt/from_host/ssh:ro -v /home/$USER/.aws/:/opt/from_host/aws:ro   seqware/launcher /bin/bash
-# Once you are inside the container, you must copy /opt/ssh/$PEM_KEY to /home/ubuntu/.ssh and run "chmod og-r $PEM_KEY". Then it's business as usuall, for a Launcher node.
+docker run -i -t -P --privileged=true --name launcher \
+        -v /home/$USER/ssh_for_docker:/opt/from_host/ssh:ro \
+        -v /home/$USER/.aws/:/opt/from_host/aws:ro \
+        -v /etc/localtime:/etc/localtime:ro \
+        seqware/launcher:$IMAGE_VERSION /bin/bash
+# Once you are inside the container, you must copy /opt/ssh/$PEM_KEY to /home/ubuntu/.ssh and run "chmod og-r $PEM_KEY". Also,
+# be sure to copy your aws config files for youxia. Then it's business as usual, for a Launcher node.
+
