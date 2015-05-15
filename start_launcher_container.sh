68658280c4728ee62cd4b3a7d88106807beb2d95
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-05-15 17:16:00 -0400

    Changed name of image

--- start_launcher_container.sh
+++ start_launcher_container.sh
@@ -6,14 +6,14 @@ IMAGE_VERSION=$2
 # Create a folder that will be mounted into the docker container
 [[ -d ~/ssh_for_docker ]] || mkdir ~/ssh_for_docker
 # Copy the pem file in $1 to the folder for the container.
-[[ -f ~/ssh_for_docker/$(basname $PEM_KEY) ]] || cp $PEM_KEY ~/ssh_for_docker/$(basename $PEM_KEY)
+[[ -f ~/ssh_for_docker/$(basename $PEM_KEY) ]] || cp $PEM_KEY ~/ssh_for_docker/$(basename $PEM_KEY)
 # After running this command, you will have to run "sudo docker attach launcher" to get into the container.
-docker run -i -t -P --privileged=true --name launcher \
+docker run -i -t -P --privileged=true --name pancancer_launcher \
         -v /home/$USER/ssh_for_docker:/opt/from_host/ssh:ro \
         -v /home/$USER/.aws/:/opt/from_host/aws:ro \
         -v /etc/localtime:/etc/localtime:ro \
         -p 15627:15672 \
-        pancancer/launcher:$IMAGE_VERSION /bin/bash
+        pancancer/pancancer_launcher:$IMAGE_VERSION /bin/bash
 # Once you are inside the container, you must copy /opt/ssh/$PEM_KEY to /home/ubuntu/.ssh and run "chmod og-r $PEM_KEY". Also,
 # be sure to copy your aws config files for youxia. Then it's business as usual, for a Launcher node.
 
