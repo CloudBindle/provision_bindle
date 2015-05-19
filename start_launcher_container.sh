de4750ccd3e68469e7974c7304036520a47ad958
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-05-19 17:42:37 -0400

    Update port mappings for container

--- start_launcher_container.sh
+++ start_launcher_container.sh
@@ -12,6 +12,9 @@ docker run -i -t -P --privileged=true --name pancancer_launcher \
         -v /home/$USER/ssh_for_docker:/opt/from_host/ssh:ro \
         -v /home/$USER/.aws/:/opt/from_host/aws:ro \
         -v /etc/localtime:/etc/localtime:ro \
+        -p 5672:5672 \
+        -p 4567:4657 \
+        -p 8080:8080 \
         -p 15672:15672 \
         pancancer/pancancer_launcher:$IMAGE_VERSION /bin/bash
 # Once you are inside the container, you must copy /opt/ssh/$PEM_KEY to /home/ubuntu/.ssh and run "chmod og-r $PEM_KEY". Also,
