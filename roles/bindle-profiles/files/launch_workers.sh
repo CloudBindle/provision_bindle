de58f006860cffd9255fe7a3c5c74d38776dd4f9
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-07-29 16:07:40 -0400

    Increase the sleep

--- roles/bindle-profiles/files/launch_workers.sh
+++ roles/bindle-profiles/files/launch_workers.sh
@@ -21,16 +21,16 @@ fi
 # Each command is preceeded by a sleep because it seems that the database might not have finished starting by the time these commands are executed.
 
 # run the Generator
-sleep 5
+sleep 60
 Generator --workflow-name HelloWorld --workflow-version 1.0-SNAPSHOT --workflow-path /workflows/Workflow_Bundle_HelloWorld_1.0-SNAPSHOT_SeqWare_1.1.0 --config ~/arch3/config/masterConfig.ini --total-jobs 1
 
 #Run the Coordinator
-sleep 5
+sleep 60
 Coordinator --config config/masterConfig.ini
 cat coordinator.out
 
 #Run the Provisioner
-sleep 5
+sleep 60
 Provisioner --config config/masterConfig.ini
 cat provisioner.out
 
