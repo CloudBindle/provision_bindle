7029e2ea4fc71f7cff92e323be8cecf22efedad3
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-09-09 16:13:00 -0400

    Fix order of args to python script; fix path to log

--- roles/bindle-profiles/files/start_services_in_container.sh
+++ roles/bindle-profiles/files/start_services_in_container.sh
@@ -12,7 +12,7 @@ Setting up. This may take a few moments...
 
 INTRO_MESSAGE
 
-exec 3>&1 4>&2 1>>startup.log 2>&1
+exec 3>&1 4>&2 1>>~/startup.log 2>&1
 echo -e "\n\n[BEGIN: $(date +%Y-%m-%d_%H:%M:%S)]"
 
 # Copy pem keys and other config files from the host.
@@ -33,7 +33,7 @@ if [ "$HOST_ENV" == "AWS" ] ; then
   echo "Querying AWS for public IP address of this machine..."
   export PUBLIC_IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
   export SENSU_SERVER_IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-  python update_security_groups.py $PUBLIC_IP_ADDRESS $HOST_INSTANCE_ID
+  python ~/update_security_groups.py $HOST_INSTANCE_ID $PUBLIC_IP_ADDRESS
 elif [ "$HOST_ENV" == "OPENSTACK" ] ; then
   # Looks like the OpenStack metadata IP address is the same as AWS
   echo "Querying OpenStack for public IP address of this machine..."
